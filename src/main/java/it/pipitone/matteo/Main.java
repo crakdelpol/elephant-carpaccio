701fd2c61fc019b3cb360c2f2cd25792b8f88a69
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-11-17 16:37:49 +0100

    print first parameter

--- src/main/java/it/pipitone/matteo/Main.java
+++ src/main/java/it/pipitone/matteo/Main.java
@@ -3,7 +3,8 @@ package it.pipitone.matteo;
 public class Main {
 
     public static void main(String[]args){
-        System.out.println("hello world");
+
+        System.out.println("first parameter " + args[0]);
     }
 
 }
