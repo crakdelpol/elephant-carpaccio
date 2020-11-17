4aac6572ee6b037f7bb6d463b8c7410bcf109130
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-11-17 17:22:49 +0100

    add tax matrix information

--- src/main/java/it/pipitone/matteo/Order.java
+++ src/main/java/it/pipitone/matteo/Order.java
@@ -2,12 +2,20 @@ package it.pipitone.matteo;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
 
 public class Order {
 
     private final BigDecimal itemsNumber;
     private final BigDecimal price;
     private final BigDecimal tax;
+    private final HashMap<String, BigDecimal> taxMatrix = new HashMap<String, BigDecimal>(){{
+        put("UT", BigDecimal.valueOf(6.85));
+        put("NV", BigDecimal.valueOf(8));
+        put("TX", BigDecimal.valueOf(6.25));
+        put("AL", BigDecimal.valueOf(4));
+        put("CA", BigDecimal.valueOf(8.25));
+    }};
 
     public Order(String firstParameter, String secondParameter, String thirdParameter) {
         this.itemsNumber = new BigDecimal(firstParameter);
