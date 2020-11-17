9e82c597f0c5dbe1c682e8bf11d61706301f751a
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-11-17 18:09:38 +0100

    Implement all discount size

--- src/main/java/it/pipitone/matteo/DiscountCalculator.java
+++ src/main/java/it/pipitone/matteo/DiscountCalculator.java
@@ -7,13 +7,23 @@ public class DiscountCalculator {
     }
 
     protected  BigDecimal calculateDiscount(BigDecimal amount){
-        if(amount.compareTo(BigDecimal.valueOf(1000)) > 0){
-            return calculateDiscount(amount, BigDecimal.valueOf(.03));
+
+        if(amount.compareTo(BigDecimal.valueOf(50000)) >= 0){
+            return amount.multiply(BigDecimal.ONE.subtract(new BigDecimal(".15")));
+        }
+        if(amount.compareTo(BigDecimal.valueOf(10000)) >= 0){
+            return amount.multiply(BigDecimal.ONE.subtract(new BigDecimal(".10")));
+
+        }if(amount.compareTo(BigDecimal.valueOf(7000)) >= 0){
+            return amount.multiply(BigDecimal.ONE.subtract(new BigDecimal(".07")));
+        }
+        if(amount.compareTo(BigDecimal.valueOf(5000)) >= 0){
+            return amount.multiply(BigDecimal.ONE.subtract(new BigDecimal(".05")));
+        }
+        if(amount.compareTo(BigDecimal.valueOf(1000)) >= 0){
+            return amount.multiply(BigDecimal.ONE.subtract(new BigDecimal(".03")));
         }
         return amount;
     }
 
-    private BigDecimal calculateDiscount(BigDecimal amount, BigDecimal discountPercent) {
-        return amount.multiply(BigDecimal.ONE.subtract(discountPercent));
-    }
 }
--- src/test/java/it/pipitone/matteo/DiscountCalculatorTest.java
+++ src/test/java/it/pipitone/matteo/DiscountCalculatorTest.java
@@ -24,4 +24,34 @@ public class DiscountCalculatorTest {
         BigDecimal totalWithDiscount = discountCalculator.calculateDiscount(amount);
         assertThat(totalWithDiscount, Matchers.comparesEqualTo(BigDecimal.valueOf(1940)));
     }
+
+    @Test
+    public void amountGreaterThan1000DiscountIs5Percent() {
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        BigDecimal amount = BigDecimal.valueOf(5000);
+        BigDecimal totalWithDiscount = discountCalculator.calculateDiscount(amount);
+        assertThat(totalWithDiscount, Matchers.comparesEqualTo(BigDecimal.valueOf(4750)));
+    }
+    @Test
+    public void amountGreaterThan1000DiscountIs7Percent() {
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        BigDecimal amount = BigDecimal.valueOf(7000);
+        BigDecimal totalWithDiscount = discountCalculator.calculateDiscount(amount);
+        assertThat(totalWithDiscount, Matchers.comparesEqualTo(BigDecimal.valueOf(6510)));
+    }
+    @Test
+    public void amountGreaterThan1000DiscountIs10Percent() {
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        BigDecimal amount = BigDecimal.valueOf(10000);
+        BigDecimal totalWithDiscount = discountCalculator.calculateDiscount(amount);
+        assertThat(totalWithDiscount, Matchers.comparesEqualTo(BigDecimal.valueOf(9000)));
+    }
+    @Test
+    public void amountGreaterThan1000DiscountIs15Percent() {
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        BigDecimal amount = BigDecimal.valueOf(50000);
+        BigDecimal totalWithDiscount = discountCalculator.calculateDiscount(amount);
+        assertThat(totalWithDiscount, Matchers.comparesEqualTo(BigDecimal.valueOf(42500)));
+    }
+
 }
