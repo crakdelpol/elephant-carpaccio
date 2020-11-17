d6e78cdb84118f9fb30b49d850ab8436610fa29d
Matteo Pipitone <matteo.pipitone@gmail.com>
2020-11-17 17:17:51 +0100

    remove method not used

--- src/main/java/it/pipitone/matteo/Order.java
+++ src/main/java/it/pipitone/matteo/Order.java
@@ -4,13 +4,10 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Order {
+
     private final BigDecimal itemsNumber;
     private final BigDecimal price;
     private final BigDecimal tax;
-
-    protected static BigDecimal addTaxToTotalAmount(BigDecimal totalAmount) {
-        return totalAmount.multiply(BigDecimal.valueOf(1.10));
-    }
 
     public Order(String firstParameter, String secondParameter, String thirdParameter) {
         this.itemsNumber = new BigDecimal(firstParameter);
@@ -26,9 +23,4 @@ public class Order {
         return calculateTotalOfProduct().multiply(BigDecimal.valueOf(100).add(tax)).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
     }
 
-    protected static BigDecimal productOfParameters(String firstParameter, String secondParameter) {
-        BigDecimal itemsNumber = new BigDecimal(firstParameter);
-        BigDecimal price = new BigDecimal(secondParameter);
-        return itemsNumber.multiply(price);
-    }
 }
